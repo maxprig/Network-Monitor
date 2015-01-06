2ccb80d1629b086c54ad1af0413ef3dd66d905d7
Maxim Prigozhenkov <maxprig@gmail.com>
2015-01-06 12:50:44 +0300

    Реализован класс "HostObject". Данный класс определяет доступность хоста с помощью объекта класса HostClaim. Так же объект этого класса с помощью многократной проверки может заключить, доступен хост в текущий момент времени или нет.

---
+++ Network Monitor/HostObject.h
@@ -0,0 +1,34 @@
+//
+//  HostObject.h
+//  Network Monitor
+//
+//  Created by Максим on 13.12.14.
+//  Copyright (c) 2014 Prigozhenkov Maxim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HostObject : NSObject
+
+//Значение переменной свидетельствует о доступности хоста
+@property (assign) BOOL hostStatus;
+
+//Адрес хоста
+@property (assign) 	NSString *hostAddress;
+
+//Время последнего ответа
+@property (assign) NSString *lastTime;
+
+//Последнее время, когда хост был доступен
+@property (assign) NSString *lastOnlineTime;
+
+//Время потери соединения
+@property (assign) NSString *lostConnectionWithHostTime;
+
+-(id)initWithAddress:(NSString *)address;
+
+-(void)doConnection;
+
+-(void)notification;
+
+@end
---
+++ Network Monitor/HostObject.m
@@ -0,0 +1,65 @@
+//
+//  HostObject.m
+//  Network Monitor
+//
+//  Created by Максим on 13.12.14.
+//  Copyright (c) 2014 Prigozhenkov Maxim. All rights reserved.
+//
+
+#import "HostObject.h"
+#import "HostClaim.h"
+
+@implementation HostObject
+
+@synthesize hostStatus;
+@synthesize hostAddress;
+@synthesize lastTime;
+@synthesize lastOnlineTime;
+@synthesize lostConnectionWithHostTime;
+
+//Создадим объект класса HostClaim. В инициализаторе позже его создадим, так как с ним придется постоянно работать.
+HostClaim *claimToHost = nil;
+
+#pragma mark -Инициализатор-
+- (id)initWithAddress:(NSString *)address
+{
+    self = [super init];
+    if (self) {
+        hostAddress = address;
+        hostStatus = false;
+        claimToHost = [[HostClaim alloc]initWithAddress:address];
+    }
+    return self;
+}
+
+#pragma mark -Проверка доступности хоста-
+-(void)doConnection{
+    [claimToHost claimHostToAddress];
+    if ([claimToHost onlineHost]) {
+        hostStatus = true;
+    }else if (![claimToHost onlineHost]){
+        //Если нет ответа от хоста, то
+        //Будем пытаться повторно соединиться пять раз.
+        //Если за пять попыток соединения мы не получим ответа, то хост ушел в оффлайн.
+        for (int i = 0; i <= 5; i++) {
+            NSLog(@"Попытка соединения №%i", i);
+            [claimToHost claimHostToAddress];
+            //Проверка, если вдруг очнется
+            if ([claimToHost onlineHost]) {
+                hostStatus = true;
+                break;
+            }
+            //Если ответ так и не поступил
+            if (![claimToHost onlineHost]&& i == 5 ) {
+                hostStatus = false;
+            }
+            sleep(10); //Ожидаем 10 секунд до следующей попытки
+        }
+    }
+}
+
+
+-(void)notification{
+    //Не знаю пока, что тут будет, но пусть будет.
+}
+@end
