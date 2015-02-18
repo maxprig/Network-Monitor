cd2638c8b75565e43b6e1f8295dc86e67d810367
Maxim Prigozhenkov <maxprig@gmail.com>
2015-02-18 22:17:14 +0300

    Добавлено новое окно и класс для него

---
+++ Network Monitor/AddGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddGroupViewController.h
+//  Network Monitor
+//
+//  Created by Максим on 18.02.15.
+//  Copyright (c) 2015 Prigozhenkov Maxim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AddGroupViewController : NSViewController
+
+@end
