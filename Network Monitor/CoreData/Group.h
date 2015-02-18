9bf7210ca409dbf93f62f14a3e456a89af04cc27
Maxim Prigozhenkov <maxprig@gmail.com>
2015-02-18 21:50:30 +0300

    Добавлены сущности и связи в CoreData.

---
+++ Network Monitor/CoreData/Group.h
@@ -0,0 +1,28 @@
+//
+//  Group.h
+//  Network Monitor
+//
+//  Created by Максим on 18.02.15.
+//  Copyright (c) 2015 Prigozhenkov Maxim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class HostList;
+
+@interface Group : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * groupID;
+@property (nonatomic, retain) NSSet *hostList;
+@end
+
+@interface Group (CoreDataGeneratedAccessors)
+
+- (void)addHostListObject:(HostList *)value;
+- (void)removeHostListObject:(HostList *)value;
+- (void)addHostList:(NSSet *)values;
+- (void)removeHostList:(NSSet *)values;
+
+@end
---
+++ Network Monitor/CoreData/Group.m
@@ -0,0 +1,19 @@
+//
+//  Group.m
+//  Network Monitor
+//
+//  Created by Максим on 18.02.15.
+//  Copyright (c) 2015 Prigozhenkov Maxim. All rights reserved.
+//
+
+#import "Group.h"
+#import "HostList.h"
+
+
+@implementation Group
+
+@dynamic name;
+@dynamic groupID;
+@dynamic hostList;
+
+@end
