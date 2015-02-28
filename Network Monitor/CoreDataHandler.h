2bf0b6efe824490786134a271e8f77a59340fd6f
Maxim Prigozhenkov <maxprig@gmail.com>
2015-03-01 00:09:32 +0300

    Добавлен синглтон для взаимодействия с  CoreData

---
+++ Network Monitor/CoreDataHandler.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataHandler.h
+//  Network Monitor
+//
+//  Created by Максим on 01.03.15.
+//  Copyright (c) 2015 Prigozhenkov Maxim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataHandler : NSObject
+
+@end
